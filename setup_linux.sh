beb6614b0d0d10ce9036620db9f0e9a5e4aad649
Simon Hofmann <simon.hofmann@consol.de>
2019-05-14 11:19:52 +0200

    Switched to bsdtar by default to fix symlink problems in resulting package

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,3 +9,6 @@ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
 # install cmake
 sudo apt-get install cmake
+# install bsdtar, required since npm does not properly unpack symlinks and breaks linking
+# bsdtar just copies link targets, which results in a larger package but does not break linking
+sudo apt-get install bsdtar
