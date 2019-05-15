7e519d3e0781bc04c18b5c6ebd689c5a0d41a5d6
Simon Hofmann <simon.hofmann@consol.de>
2019-05-15 21:30:34 +0200

    Disabled pre-installed image libs

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo apt-get update
 # install tools for reading images from disk
-sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+# sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
 # install video codecs
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
