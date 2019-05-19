9e803926b1ec47cf75bf4716e75d08f9dc2e2eb7
Simon Hofmann <simon.hofmann@consol.de>
2019-05-19 21:19:02 +0200

    Removed 3rdparty folder, updated package scripts

--- prepublish.js
+++ prepublish.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+
 const filename = "./package.json";
 const packageJson = require(filename);
 
@@ -6,9 +7,9 @@ const plattformPackageName = `@nut-tree/opencv-build-${process.platform}`;
 packageJson.name = plattformPackageName;
 
 try {
-  fs.writeFileSync(filename, JSON.stringify(packageJson, null, 2));
+    fs.writeFileSync(filename, JSON.stringify(packageJson, null, 2));
 } catch (err) {
-  console.log(err);
-  process.exit(-1);
+    console.log(err);
+    process.exit(-1);
 }
 console.log(`Patched package name to '${plattformPackageName}'`);
